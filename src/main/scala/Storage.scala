785c08ffbed618a2bcef3045bfd0900639cb0c98
Mark Canlas <github@htmlism.com>
2018-07-27 18:42:01 -0400

    added thoughts

--- src/main/scala/Storage.scala
+++ src/main/scala/Storage.scala
@@ -13,8 +13,35 @@ class Storage[F[_] : Sync] {
   private val books   = collection.mutable.Map.empty[Int, Book]
   private val authors = collection.mutable.Map.empty[Int, Author]
 
+  /**
+   * Remove FKs from child objects so that their linkage to their parents can happen more intentionally rather than
+   * randomly. This is the separation between real data and synthetic data (Fks)
+   *
+   * Entity[A] // A is given primary key
+   * HasForeignKey[A] // A is given foreign key, to what? maybe extend with second type parameter
+   * A References B == References[A, B] A is "under" B, either 1 to 1 or children of B
+   *
+   * --
+   *
+   * There must be an orchestration type to signify a relationship between two types, to differentiate it from a tuple.
+   * Orchestrated[A, B] vs (A, B)
+   *
+   * --
+   *
+   * (K, Option[A}), List[(K, A)]
+   *
+   * Where none the second part specifically does not satisfy for the `K` given in the first part.
+   *
+   * Where all the A can be stacked monadically
+   * @return
+   */
   def selectOne = ???
 
+  /**
+   * type Grp = (A, List[B])
+   *
+   * (K, Grp), List[(K, Grp))
+   */
   def selectOneToMany(authorId: Int): F[List[(Author, Book)]] =
     Sync[F].delay {
       for {
