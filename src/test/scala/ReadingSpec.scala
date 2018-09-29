8305b28897a8e3edbd99f7f4ec91ff8641558cba
Mark Canlas <github@htmlism.com>
2018-09-29 09:15:33 -0400

    120 supremacy

--- src/test/scala/ReadingSpec.scala
+++ src/test/scala/ReadingSpec.scala
@@ -1,7 +1,4 @@
 import org.scalatest._
 import org.scalatest.prop._
 
-class ReadingSpec
-    extends PropSpec
-    with Matchers
-    with GeneratorDrivenPropertyChecks {}
+class ReadingSpec extends PropSpec with Matchers with GeneratorDrivenPropertyChecks {}
--- src/test/scala/WritingSpec.scala
+++ src/test/scala/WritingSpec.scala
@@ -1,7 +1,4 @@
 import org.scalatest._
 import org.scalatest.prop._
 
-class WritingSpec
-    extends PropSpec
-    with Matchers
-    with GeneratorDrivenPropertyChecks {}
+class WritingSpec extends PropSpec with Matchers with GeneratorDrivenPropertyChecks {}
